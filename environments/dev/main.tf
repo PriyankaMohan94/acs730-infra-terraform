2716ddeeabbe32629e44a45a200a4c27d65fc259
PriyankaMohan94 <mpriyankaam@gmail.com>
2025-04-14 01:45:21 +0000

    Add Security Group module and usage in dev

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,3 +11,8 @@ module "vpc" {
   project              = "Group1Dev"
 }
 
+module "security_group" {
+  source  = "../../modules/security-group"
+  vpc_id  = module.vpc.vpc_id
+  project = "Group1Dev"
+}
