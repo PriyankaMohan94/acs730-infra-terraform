400045cf196f4428fec671531a8bdd5a1022fcfe
Dineshkumarmjb <dineshkumarmjb@gmail.com>
2025-04-14 20:51:41 +0000

    Fix: terraform fmt formatting issues

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,10 +6,10 @@ module "vpc" {
   source   = "../../modules/vpc"
   vpc_cidr = "10.1.0.0/16"
   public_subnet_cidrs = [
-    "10.1.1.0/24", 
-    "10.1.2.0/24", 
-    "10.1.3.0/24", 
-    "10.1.4.0/24"  
+    "10.1.1.0/24",
+    "10.1.2.0/24",
+    "10.1.3.0/24",
+    "10.1.4.0/24"
   ]
   private_subnet_cidrs = ["10.1.5.0/24", "10.1.6.0/24"]
   azs                  = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
