0fa324b3f97faefd946d581a432f6c2ba424e460
Dineshkumarmjb <dineshkumarmjb@gmail.com>
2025-04-14 20:04:56 +0000

    Updated CIDR of VPC

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,16 +3,16 @@ provider "aws" {
 }
 
 module "vpc" {
-  source               = "../../modules/vpc"
-  vpc_cidr             = "10.0.0.0/16"
- public_subnet_cidrs  = [
-  "10.1.1.0/24", # AZ1
-  "10.1.2.0/24", # AZ2
-  "10.1.3.0/24", # AZ3
-  "10.1.4.0/24"  # AZ4
-]
-  private_subnet_cidrs = ["10.1.5.0/24","10.1.6.0/24"]
-  azs = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  source   = "../../modules/vpc"
+  vpc_cidr = "10.1.0.0/16"
+  public_subnet_cidrs = [
+    "10.1.1.0/24", 
+    "10.1.2.0/24", 
+    "10.1.3.0/24", 
+    "10.1.4.0/24"  
+  ]
+  private_subnet_cidrs = ["10.1.5.0/24", "10.1.6.0/24"]
+  azs                  = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
   project              = "Group1Dev"
 }
 
@@ -21,30 +21,30 @@ module "security_group" {
   vpc_id  = module.vpc.vpc_id
   project = "Group1Dev"
 }
-  
+
 
 module "webservers" {
-  source              = "../../modules/webserver"
-  instance_count      = 2
-  ami_id              = "ami-0c101f26f147fa7fd"
-  instance_type       = "t2.micro"
-  subnet_ids          = [module.vpc.public_subnets[0], module.vpc.public_subnets[2]]
-  key_name            = "acs730-key"
-  security_group_ids  = [module.security_group.web_sg_id]
-  project             = "Group1Dev-Webservers"
-  assign_public_ip    = true
+  source             = "../../modules/webserver"
+  instance_count     = 2
+  ami_id             = "ami-0c101f26f147fa7fd"
+  instance_type      = "t2.micro"
+  subnet_ids         = [module.vpc.public_subnets[0], module.vpc.public_subnets[2]]
+  key_name           = "acs730-key"
+  security_group_ids = [module.security_group.web_sg_id]
+  project            = "Group1Dev-Webservers"
+  assign_public_ip   = true
 }
 
 module "bastion" {
-  source              = "../../modules/webserver"
-  instance_count      = 1
-  ami_id              = "ami-0c101f26f147fa7fd"
-  instance_type       = "t2.micro"
-  subnet_ids          = [module.vpc.public_subnets[1]]
-  key_name            = "acs730-key"
-  security_group_ids  = [module.security_group.web_sg_id]
-  project             = "Group1Dev-Bastion"
-  assign_public_ip    = true
+  source             = "../../modules/webserver"
+  instance_count     = 1
+  ami_id             = "ami-0c101f26f147fa7fd"
+  instance_type      = "t2.micro"
+  subnet_ids         = [module.vpc.public_subnets[1]]
+  key_name           = "acs730-key"
+  security_group_ids = [module.security_group.web_sg_id]
+  project            = "Group1Dev-Bastion"
+  assign_public_ip   = true
 }
 
 module "alb" {
@@ -56,37 +56,37 @@ module "alb" {
   web_instance_ids  = module.webservers.instance_ids
 }
 module "webserver4" {
-  source              = "../../modules/webserver"
-  instance_count      = 1
-  ami_id              = "ami-0c101f26f147fa7fd"
-  instance_type       = "t2.micro"
-  subnet_ids          = [module.vpc.public_subnets[3]]
-  key_name            = "acs730-key"
-  security_group_ids  = [module.security_group.web_sg_id]
-  project             = "Group1Dev-Webserver4"
-  assign_public_ip    = true
+  source             = "../../modules/webserver"
+  instance_count     = 1
+  ami_id             = "ami-0c101f26f147fa7fd"
+  instance_type      = "t2.micro"
+  subnet_ids         = [module.vpc.public_subnets[3]]
+  key_name           = "acs730-key"
+  security_group_ids = [module.security_group.web_sg_id]
+  project            = "Group1Dev-Webserver4"
+  assign_public_ip   = true
 }
 
 module "DBserver" {
-  source              = "../../modules/webserver"
-  instance_count      = 1
-  ami_id              = "ami-0c101f26f147fa7fd"
-  instance_type       = "t2.micro"
-  subnet_ids          = [module.vpc.private_subnets[0]]
-  key_name            = "acs730-key"
+  source             = "../../modules/webserver"
+  instance_count     = 1
+  ami_id             = "ami-0c101f26f147fa7fd"
+  instance_type      = "t2.micro"
+  subnet_ids         = [module.vpc.private_subnets[0]]
+  key_name           = "acs730-key"
   security_group_ids = [module.security_group.private_sg_id]
-  project             = "Group1Dev-Webserver5"
-  assign_public_ip    = false
+  project            = "Group1Dev-Webserver5"
+  assign_public_ip   = false
 }
 
 module "webserver6" {
-  source              = "../../modules/webserver"
-  instance_count      = 1
-  ami_id              = "ami-0c101f26f147fa7fd"
-  instance_type       = "t2.micro"
-  subnet_ids          = [module.vpc.private_subnets[1]]  
-  key_name            = "acs730-key"
-  security_group_ids  = [module.security_group.private_sg_id]
-  project             = "Group1Dev-DBserver"
-  assign_public_ip    = false
+  source             = "../../modules/webserver"
+  instance_count     = 1
+  ami_id             = "ami-0c101f26f147fa7fd"
+  instance_type      = "t2.micro"
+  subnet_ids         = [module.vpc.private_subnets[1]]
+  key_name           = "acs730-key"
+  security_group_ids = [module.security_group.private_sg_id]
+  project            = "Group1Dev-DBserver"
+  assign_public_ip   = false
 }
